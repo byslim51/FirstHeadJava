072e3c9c7d8383b6ae8de0a771f9337f133e2a59
Slim <you@example.com>
2024-07-11 12:33:02 +0500

    Вторая "задача" из книги HFJ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.io.FileReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        try {
+            Integer.parseInt("a");
+        } catch (NumberFormatException n){
+            System.out.println(n.getMessage());
+
+        }
         System.out.println("hello");
     }
 }
