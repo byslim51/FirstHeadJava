c3dea7acf5ba4e45c51435b9b83ff9930e3df538
Slim <you@example.com>
2024-07-11 14:34:56 +0500

    1 Задача (1 глава)

---
+++ src/chapter1/Task1.java
@@ -0,0 +1,31 @@
+// 50 страница
+package chapter1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int x = 3;
+
+        while (x > 0) {
+
+            if (x > 2) {
+                System.out.print("a");
+            }
+
+            x = x - 1;
+            System.out.print("-");
+
+            if (x == 2) {
+                System.out.print("b c");
+            }
+
+            x = x - 1;
+            System.out.print("-");
+
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
