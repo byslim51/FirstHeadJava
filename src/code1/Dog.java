d020bdc9533abe3fac627c0266173925e9bd5077
Slim <you@example.com>
2024-07-14 21:01:33 +0500

    Первый "Наточи свой карандаш" (3 глава) Код из примера (создание объектов, примеры класса Dog) (3 глава)

---
+++ src/code1/Dog.java
@@ -0,0 +1,34 @@
+package code1;
+
+public class Dog {
+    String name;
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.bark();
+        dog1.name = "Барт";
+
+        Dog[] myDogs = new Dog[3];
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+        myDogs[0].name = "Фред";
+        myDogs[1].name = "Джордж";
+
+        System.out.println("Имя последней собаки - " + myDogs[2].name);
+
+        for (int i = 0; i < myDogs.length; i++) {
+            myDogs[i].bark();
+        }
+
+    }
+
+    public void bark() {
+        System.out.println(name + " сказал Гав!");
+    }
+
+    public void eat() {}
+
+    public void chaseCat() {}
+}
